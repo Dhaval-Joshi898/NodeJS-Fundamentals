614a44cf99967bcb5ab0372fc15a3566115a3e0b
Dhaval <joshidhaval2002@gmail.com>
2023-10-09 19:43:51 +0530

    js single threaded

--- blockingCode.js
+++ blockingCode.js
@@ -1,5 +1,8 @@
+//Since JS is a single thread it will execute line by line
 for(let i=0 ;i<=10000000000;i++){
     //Here this doing nothing it is JUST consuming time
 }
 
-console.log('After the execution of for loop this will execute')
+console.log('Task to be performed after the loop')
+
+console.log("It has to wait for so long to finish the loop")
