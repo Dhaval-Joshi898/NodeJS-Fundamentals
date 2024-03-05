00284cced33ad118093a9f8530eca3d9e8230972
Dhaval <joshidhaval2002@gmail.com>
2024-03-06 02:46:07 +0530

    Creating a server

---
+++ Creating a Server/server.js
@@ -0,0 +1,18 @@
+//Creating a Server Using NodeJS
+
+//Step 1 Import http library/module
+var http=require("http")
+
+
+//Step 2. Create a Server
+const server=http.createServer((req,res)=>{   //createServer return a instance of server ,therefore it needs to be stored in variable 
+    //Here comes the req/response       
+    res.end('HEllo welcome to the server')
+})
+
+
+//Step 3. Specify a port number to listen to Client's Request
+
+server.listen(3002,()=>{  
+  console.log("Server listening on port 3001")  
+})
