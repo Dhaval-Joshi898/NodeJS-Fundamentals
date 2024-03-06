2034978852a6c521dd9a10fb056639ae87a64fb9
Dhaval <joshidhaval2002@gmail.com>
2024-03-06 23:49:44 +0530

    added hostname and port as parameter

--- Creating a Server/server.js
+++ Creating a Server/server.js
@@ -9,10 +9,11 @@ const server=http.createServer((req,res)=>{   //createServer return a instance o
     //Here comes the req/response       
     res.end('HEllo welcome to the server')
 })
-
-
+const hostname='127.0.0.1'
+const port=3001
 //Step 3. Specify a port number to listen to Client's Request
 
-server.listen(3001,()=>{  
-  console.log("Server listening on port 3001")  
+server.listen(port,hostname,()=>{  
+  console.log("Server listening on port 3001") 
+  console.log(`http://${hostname}:${port}`) 
 }) //the other parameter after port number(3000) is a callback function ,and this callback function will be executed when server has started to listen
