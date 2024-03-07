2569783e18440972bfbffa2fd4393de9d7074c61
Dhaval <joshidhaval2002@gmail.com>
2024-03-07 22:47:26 +0530

    file moved to different folder

--- firstNode.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World")
-setTimeout(()=>{
-    console.log("After 8 seconds running")
-},8000)
-
-console.log("will run before 8 seconds")
-
--- test.js
+++
@@ -1,6 +0,0 @@
-let i=0;
-setInterval(()=>{
-  i+=1;
-  // console.log(i)
-},1000)
-console.log(i)
