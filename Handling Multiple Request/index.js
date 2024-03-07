245151f1e4a27df0b215414e1d67a7c1298659eb
Dhaval <joshidhaval2002@gmail.com>
2024-03-07 19:15:51 +0530

    return kkeyword use for editing the function

--- Handling Multiple Request/index.js
+++ Handling Multiple Request/index.js
@@ -6,7 +6,7 @@ const server=http.createServer((req,res)=>{
     if(req.url==='/user'){  //if in url it is mentioned 8000/user then send this below response
        return res.end("\n Welcome to User URL page REQUEST")
     //the point of using return is to exit the callback function which is this 'createServer((req,res)=>{}' so if -
-    // -conditon satisfied no need to go to elseif
+    // -conditon satisfied no need to go to elseif //NOTE:The return keyword is used for exiting the function
 
     }
     else if(req.url=='/product'){
