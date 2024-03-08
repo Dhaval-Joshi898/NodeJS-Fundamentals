2b67cd03b3c78ee5759de81ce3cbad128e112675
Dhaval <joshidhaval2002@gmail.com>
2024-03-08 20:46:51 +0530

    common js module used to export function  of this module

---
+++ Modules in Node JS PART/Understanding Module/Common JS Module/arithmatic.js
@@ -0,0 +1,14 @@
+function sum(x,y){
+    return x+y
+}
+
+function div(x,y){
+    return x/y
+}
+
+//the below is the common way to export function ,class ,variables 
+//They are export in form of object
+module.exports={
+    add:sum,
+    divide:div  //here at time of import the property(divide) will be used to access the div function
+}
