2100073828b67e6555042ecfaa8c5e2e6844111f
Dhaval <joshidhaval2002@gmail.com>
2024-03-08 23:00:43 +0530

    import directly inside this{}

--- Modules in Node JS PART/Understanding Module/ES6 Module/result1.mjs
+++ Modules in Node JS PART/Understanding Module/ES6 Module/result1.mjs
@@ -1,6 +1,10 @@
-import * as arithmaticModule from './arithmatic1.mjs'
+import * as arithmaticModule from './arithmatic1.mjs'  //here we use arithmaticModule as a object name to access property like add function
+import {multiply} from './arithmatic1.mjs'
+//we can also directly import function name,class or variables by directly importing them inside this curly brackets {}
 
-console.log(arithmaticModule.add(2,4))
+console.log("import * as arithmaticModule from './arithmatic1.mjs' ")
+console.log('arithmaticModule.add(2,4)---->',arithmaticModule.add(2,4))
 
-// console.log(multiply(2,4))
-// import {multiply} from './arithmatic1.mjs'
+console.log('-----------------------------------------------------------------')
+console.log("import {multiply} from './arithmatic1.mjs'")
+console.log('multiply(2,4)---->',multiply(2,4))
