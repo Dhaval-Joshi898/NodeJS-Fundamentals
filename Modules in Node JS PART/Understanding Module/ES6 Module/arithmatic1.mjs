da80e5639c1c51ae6f2e8359e3de2f41880ee0bf
Dhaval <joshidhaval2002@gmail.com>
2024-03-08 23:01:29 +0530

    export directly by the name inside this {multiply}

--- Modules in Node JS PART/Understanding Module/ES6 Module/arithmatic1.mjs
+++ Modules in Node JS PART/Understanding Module/ES6 Module/arithmatic1.mjs
@@ -8,3 +8,7 @@ function multiply(x,y){
     return x*y;
 }
 
+export {multiply} //another way to export inside this {}
+
+//In this way at time of import the multipy function can directly be import inside this {}  and use as it is with name 
+// LIKE before we need to use any object like as arithmaticModule form file name and  then  arithmaticModule.multiply
