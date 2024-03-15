c235605c3112fe7b3842f340815a134c74ee08d9
Dhaval <joshidhaval2002@gmail.com>
2024-03-15 12:18:00 +0530

    read file parameter information and buffer converted to string

--- File system Module/Non Blocking asynchronous file operation/index.js
+++ File system Module/Non Blocking asynchronous file operation/index.js
@@ -1,6 +1,6 @@
 const fs=require('fs')
 
-//fs.writeFile('filename','data to be added in string' and 3param iS callback function with error parameter if an error encounted whike writing in file)
+//fs.writeFile('fileName','data to be added in string' and 3param iS callback function with error parameter if an error encounted whike writing in file)
 fs.writeFile('text.txt','The content is written using writeFIle',(err)=>{
     if(err){
         console.log(err)
@@ -9,11 +9,13 @@ fs.writeFile('text.txt','The content is written using writeFIle',(err)=>{
         console.log("Data has been written on file")
     }
 })
+
+//fs.readFile('fileName',a callback function (err,buffer) with 2 params buffer is the data we get from file we need to convert it to string)
 fs.readFile('text.js',(err,data)=>{
     if(err){
         console.log(err)
     }
     else{
-        console.log(data.toString())
+        console.log(data.toString())  //converting buffer data  to string  
     }
 })
