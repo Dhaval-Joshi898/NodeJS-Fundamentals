c8091a3030a862e182c112dfe7930e458e0ea5ba
Dhaval <joshidhaval2002@gmail.com>
2024-09-27 15:53:12 +0530

    unlinkSync and try catch

--- File system Module/code Blocking approach/fileOperations.js
+++ File system Module/code Blocking approach/fileOperations.js
@@ -1,13 +1,22 @@
 const fs=require('fs')
 
 //writeFileSync will write the  data in file mentioned in first PARAMETER if file not exist it will create that file 
-fs.writeFileSync('students.txt',"Name:John Doe   Majors:Computer")   //fs.writeFileSync(filename,content to add -->in string)
+// fs.writeFileSync('students.txt',"Name:John Doe   Majors:Computer")   //fs.writeFileSync(filename,content to add -->in string)
 
 
 // appending the data in the file  if file not there it will also CREATE the file and then append it.
-fs.appendFileSync('students.txt','\nUniversity name:XYZ')
+// fs.appendFileSync('students.txt','\nUniversity name:XYZ')
 
 
 //Delete a file 
-fs.unlink('students.txt')
+fs.unlinkSync('students.txt')
+
+// try{
+//     fs.unlink('students.txt')
+
+// }
+// catch(err){
+//     console.log("Files doesn't exist")   //After deleting file we can write this error  message
+// } 
+
 console.log('Another task is performed after the file operation done')
